ae0bddb523242a720bfe4fe6619b6e8fe86d5580
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-02-11 00:58:47 +0000

    Create and test notifications model

--- peer_support/models/__init__.py
+++ peer_support/models/__init__.py
@@ -5,3 +5,4 @@ from .parent_model import Parent
 from .patient_model import Patient
 from .message_model import Message
 from .conversation_model import Conversation, GroupConversation
+from .notification_model import Notification
