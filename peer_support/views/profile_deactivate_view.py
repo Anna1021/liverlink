54ff88d146e98d8495e964f920f9ceb4bf3be8d7
Yara Niroukh <yaraniroukh@hotmail.com>
2024-02-12 22:22:57 +0000

    Implement deactivate_user view tests & change view to allow only POST requests

--- peer_support/views/profile_deactivate_view.py
+++ peer_support/views/profile_deactivate_view.py
@@ -1,13 +1,17 @@
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.http import HttpResponseForbidden
 
 def deactivate_user(request):
     """Deactivate the current user's account."""
 
-    user = request.user
-    user.is_active = False
-    user.save()
-    messages.add_message(request, messages.SUCCESS, "Profile successfully deactivated!")
-    logout(request)
-    return redirect('home')
+    if request.method == "POST":
+        user = request.user
+        user.is_active = False
+        user.save()
+        messages.add_message(request, messages.SUCCESS, "Profile successfully deactivated!")
+        logout(request)
+        return redirect('home')
+
+    return HttpResponseForbidden()
