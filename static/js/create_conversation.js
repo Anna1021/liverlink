a3726e244828e5d27caa2767d1b1c2bd41f39c0a
Hannah Porteous <hannah.porteous@yahoo.com>
2024-02-12 11:28:07 +0000

    Implement conversation creation buttons disabling

--- static/js/create_conversation.js
+++ static/js/create_conversation.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
     // Disable the buttons initially
-    $('#direct').disabled = true;
-    $('#group').disabled = true;
+    $('#direct').prop('disabled',true);
+    $('#group').prop('disabled',true);
     // Enable both buttons if 1 user selected
 
     // Disable direct message button if multiple users selected
     $('#id_users').change(function() {
       if ($(this).val().length == 0) {
-        $('#direct').disabled = true;
-        $('#group').disabled = true;
+        $('#direct').prop('disabled',true);
+        $('#group').prop('disabled',true);
       } else if ($(this).val().length == 1) {
-        $('#direct').disabled = false;
-        $('#group').disabled = false;
+        $('#direct').prop('disabled',false);
+        $('#group').prop('disabled',false);
       } else {
-        $('#direct').disabled = true;
-        $('#group').disabled = false;
+        $('#direct').prop('disabled',true);
+        $('#group').prop('disabled',false);
       }
     });
   });
