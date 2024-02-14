68b339e5c3c04d370274921b1cebb5287a9e5dc7
Riya <k22007695@kcl.ac.uk>
2024-02-14 01:13:18 +0000

    Add tests for best match

--- peer_support/tests/forms/test_sort_users_form.py
+++ peer_support/tests/forms/test_sort_users_form.py
@@ -2,7 +2,7 @@
 from django.test import TestCase
 from peer_support.forms import SortPeerForm 
 from django.test import TestCase
-from peer_support.models import User
+from peer_support.models import User,Patient,Parent
 from datetime import date
 
 class SortPeerFormTestCase(TestCase):
@@ -10,11 +10,44 @@ class SortPeerFormTestCase(TestCase):
 
     def setUp(self):
         today = date.today()
-        User.objects.create(username="@janedoe", email="janedoe@example.com", date_of_birth=date(today.year - 30, today.month, today.day))
-        User.objects.create(username="@peterpickles", email="peterpickles@example.com", date_of_birth=date(today.year - 25, today.month, today.day))
-        User.objects.create(username="@petrapickles", email="petrapickles@example.com", date_of_birth=date(today.year - 20, today.month, today.day))
+        self.current_user = Patient.objects.create(
+            username="@janedoe",
+            email="janedoe@example.com",
+            date_of_birth=date(today.year - 30, today.month, today.day),
+            gender="F",
+            location="GB",
+            hospital="Guy’s and St Thomas’ NHS Foundation Trust",
+            ethnicity="OM",
+            language="en",
+            condition="Biliary atresia",
+            age_of_diagnosis=2
+        )    
+        Patient.objects.create(
+            username="@peterpickles",
+            email="peterpickles@example.com",
+            date_of_birth=date(today.year - 25, today.month, today.day),
+            gender="F",
+            location="GB",
+            hospital="Airedale NHS Foundation Trust",
+            ethnicity="BD",
+            language="en",
+            condition="Biliary atresia",
+            age_of_diagnosis=2
+        )
+        Parent.objects.create(
+            username="@petrapickles",
+            email="petrapickles@example.com",
+            date_of_birth=date(today.year - 20, today.month, today.day),
+            gender="F",
+            location="GB",
+            hospital="Airedale NHS Foundation Trust",
+            ethnicity="BD",
+            language="en",
+            child_condition="Biliary atresia",
+            child_age_of_diagnosis=5
+        )
+        self.users = User.objects.all()
 
-        self.users = User.objects.all().order_by('id')
 
     def test_form_has_necessary_fields(self):
         form = SortPeerForm()
@@ -29,7 +62,7 @@ class SortPeerFormTestCase(TestCase):
         form_data = {'sort_by': 'username_asc'}
         form = SortPeerForm(data=form_data)
         self.assertTrue(form.is_valid())
-        sorted_users = form.sort_users(self.users)
+        sorted_users = form.sort_users(self.users, self.current_user)
         self.assertEqual(sorted_users[0].username, "@janedoe")
         self.assertEqual(sorted_users[1].username, "@peterpickles")
         self.assertEqual(sorted_users[2].username, "@petrapickles")
@@ -38,7 +71,7 @@ class SortPeerFormTestCase(TestCase):
         form_data = {'sort_by': 'username_desc'}
         form = SortPeerForm(data=form_data)
         self.assertTrue(form.is_valid())
-        sorted_users = form.sort_users(self.users)
+        sorted_users = form.sort_users(self.users, self.current_user)
         self.assertEqual(sorted_users[0].username, "@petrapickles")
         self.assertEqual(sorted_users[1].username, "@peterpickles")
         self.assertEqual(sorted_users[2].username, "@janedoe")
@@ -47,7 +80,7 @@ class SortPeerFormTestCase(TestCase):
         form_data = {'sort_by': 'age_asc'}
         form = SortPeerForm(data=form_data)
         self.assertTrue(form.is_valid())
-        sorted_users = form.sort_users(self.users)
+        sorted_users = form.sort_users(self.users, self.current_user)
         self.assertEqual(sorted_users[0].username, "@petrapickles")
         self.assertEqual(sorted_users[1].username, "@peterpickles")
         self.assertEqual(sorted_users[2].username, "@janedoe")
@@ -56,8 +89,17 @@ class SortPeerFormTestCase(TestCase):
         form_data = {'sort_by': 'age_desc'}
         form = SortPeerForm(data=form_data)
         self.assertTrue(form.is_valid())
-        sorted_users = form.sort_users(self.users)
+        sorted_users = form.sort_users(self.users, self.current_user)
         self.assertEqual(sorted_users[0].username, "@janedoe")
         self.assertEqual(sorted_users[1].username, "@peterpickles")
         self.assertEqual(sorted_users[2].username, "@petrapickles")
+    
+    def test_sort_users_by_best_match(self):
+        form_data = {'sort_by': ''}
+        form = SortPeerForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        sorted_users = form.sort_users(self.users, self.current_user)
+        expected_order = ['@janedoe', '@peterpickles', '@petrapickles']
+        sorted_usernames = [user.username for user in sorted_users]
+        self.assertEqual(sorted_usernames, expected_order)
         
