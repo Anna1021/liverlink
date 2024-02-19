d3b4aaa164c396a0a6cf5a544e6aef7f6574c58b
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-02-19 20:25:52 +0000

    Move css for friends list to seperate file

--- peer_support/views/friends_list_view.py
+++ peer_support/views/friends_list_view.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def friends_list(request):
+    """Display the list of friends."""
     user = request.user
     friends = user.friends.all()
     context = {'friends': friends}
