bd0050bcab8e710ae7f9198e94a2035c8c76457d
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-02-19 21:57:43 +0000

    Send notification when new conversation created

--- peer_support/views/create_conversation_view.py
+++ peer_support/views/create_conversation_view.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,redirect
 from django.views.generic.edit import FormView
 from django.urls import reverse
 from peer_support.forms import ConversationForm, MessageForm
+from peer_support.models import Notification
 
 class CreateConversationView(LoginRequiredMixin, FormView):
     """Displays the user's conversation"""
@@ -20,9 +21,20 @@ class CreateConversationView(LoginRequiredMixin, FormView):
         if request.POST.get('group'):
             create_group=True
         if form.is_valid():
-            conversation = form.save(request.user,create_group)
+            conversation = form.save(request.user, create_group)
+            self.send_notification(request.user, conversation)
             return redirect(reverse("conversation",kwargs={'conversation_id':conversation.id}),{'form':MessageForm(conversation,user=request.user),'conversation':conversation,'user_conversations':request.user.sort_conversations()})
         else:
             return render(request,self.template_name,{'form':form,'user_conversations':request.user.sort_conversations()})
+        
+    def send_notification(self, request_user, conversation):
+        """Sends notification to users in conversation"""
+        for user in conversation.users.all():
+            Notification.objects.create(
+                title = "New conversation",
+                description = f"You have been added to a new conversation by {request_user.username}.",
+                user = user
+            )
+
 
         
