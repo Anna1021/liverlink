017c5073c9b4dd395614f8738d4ea5a886937185
Hannah Porteous <hannah.porteous@yahoo.com>
2024-02-20 12:12:56 +0000

    Write tests

--- peer_support/views/conversation_details_view.py
+++ peer_support/views/conversation_details_view.py
@@ -11,8 +11,18 @@ class ConversationDetailsView(LoginRequiredMixin,View):
     template_name = 'conversation_details.html' 
 
     def get(self,request, conversation_id):
-        conversation = Conversation.objects.get(id=conversation_id)
+        conversations = Conversation.objects.filter(id=conversation_id)
+        if conversations.count() == 0:
+            messages.error(request,"This conversation does not exist.")
+            context = {'user_conversations':request.user.sort_conversations()}
+            return redirect(reverse('conversation',kwargs={'conversation_id':0}),context)
+        conversation = conversations.all()[0]
+        current_user = request.user
+        if current_user not in conversation.users.all():
+            messages.error(request,"You do not have access to this conversation.")
+            context = {'user_conversations':request.user.sort_conversations()}
+            return redirect(reverse('conversation',kwargs={'conversation_id':0}),context)
         if conversation.as_group() is None:
-            messages.error("You can only do this for a group conversation")
-            return redirect(reverse('conversation',kwargs={'conversation_id':conversation.id,'user_conversations':request.user.sort_conversations()}))
+            messages.error(request,"You can only do this for a group conversation")
+            return redirect(reverse('conversation',kwargs={'conversation_id':conversation.id}),{'user_conversations':request.user.sort_conversations()})
         return render(request,self.template_name,{'conversation':conversation.as_group(),'user_conversations':request.user.sort_conversations()})
