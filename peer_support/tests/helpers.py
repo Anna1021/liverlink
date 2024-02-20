71d5df845d18beb55f4888fcb055105ef9d092db
Katy Wakeman <katy.wakeman@kcl.ac.uk>
2024-02-20 14:00:09 +0000

    Fix coverage

--- peer_support/tests/helpers.py
+++ peer_support/tests/helpers.py
@@ -1,10 +1,5 @@
 from django.urls import reverse
 from with_asserts.mixin import AssertHTMLMixin
-import uuid
-from django.test import TestCase
-from peer_support.models import Mentor, Referral
-from peer_support.views.helpers import create_referral, get_referral_code
-
 
 def reverse_with_next(url_name, next_url):
     """Extended version of reverse to generate URLs with redirects"""
@@ -34,32 +29,3 @@ class MenuTesterMixin(AssertHTMLMixin):
         for url in self.menu_urls:
             self.assertNotHTML(response, f'a[href="{url}"]')
 
-class UtilsTestCase(TestCase):
-    """Unit tests for the util."""
-
-    def setUp(self):
-        self.mentor = Mentor.objects.create(username='test_mentor')
-        
-    def test_create_referral(self):
-        referral = create_referral(self.mentor)
-        self.assertIsInstance(referral, Referral)
-        self.assertEqual(referral.referrer, self.mentor)
-    
-    def test_create_referral_invalid_user(self):
-        invalid_user = 'invalid_user'
-        referral = create_referral(invalid_user)
-        self.assertIsNone(referral)
-
-    def test_get_referral_code(self):
-        code = uuid.uuid4().hex[:10].upper()
-        Referral.objects.create(referrer=self.mentor, code=code)
-        referral_code = get_referral_code(self.mentor)
-        self.assertEqual(referral_code, code)
-
-    def test_get_referral_code_no_referral(self):
-        referral_code = get_referral_code(self.mentor)
-        self.assertIsNone(referral_code)
-
-    def tearDown(self):
-        Mentor.objects.all().delete()
-        Referral.objects.all().delete()
---
+++ peer_support/tests/test_view_helpers.py
@@ -0,0 +1,34 @@
+import uuid
+from django.test import TestCase
+from peer_support.models import Mentor, Referral
+from peer_support.views.helpers import create_referral, get_referral_code
+
+class UtilsTestCase(TestCase):
+    """Unit tests for the util."""
+
+    def setUp(self):
+        self.mentor = Mentor.objects.create(username='test_mentor')
+        
+    def test_create_referral(self):
+        referral = create_referral(self.mentor)
+        self.assertIsInstance(referral, Referral)
+        self.assertEqual(referral.referrer, self.mentor)
+    
+    def test_create_referral_invalid_user(self):
+        invalid_user = 'invalid_user'
+        referral = create_referral(invalid_user)
+        self.assertIsNone(referral)
+
+    def test_get_referral_code(self):
+        code = uuid.uuid4().hex[:10].upper()
+        Referral.objects.create(referrer=self.mentor, code=code)
+        referral_code = get_referral_code(self.mentor)
+        self.assertEqual(referral_code, code)
+
+    def test_get_referral_code_no_referral(self):
+        referral_code = get_referral_code(self.mentor)
+        self.assertIsNone(referral_code)
+
+    def tearDown(self):
+        Mentor.objects.all().delete()
+        Referral.objects.all().delete()
