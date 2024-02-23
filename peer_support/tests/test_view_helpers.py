33a5d86647f3fc980cfe6151a3c8b0619f6ef2a0
Riya <k22007695@kcl.ac.uk>
2024-02-23 21:41:54 +0000

    Remove friends from peer select

--- peer_support/tests/test_view_helpers.py
+++ peer_support/tests/test_view_helpers.py
@@ -1,10 +1,16 @@
 import uuid
 from django.test import TestCase
-from peer_support.models import Mentor, Referral
-from peer_support.views.helpers import create_referral, get_referral_code
+from peer_support.models import Mentor, Referral, User
+from peer_support.views.helpers import create_referral, get_referral_code, get_addable_peers
 
 class HelpersViewTestCase(TestCase):
     """Unit tests for the helpers view."""
+
+    fixtures = [
+        'peer_support/tests/fixtures/default_admin.json',
+        'peer_support/tests/fixtures/other_users.json',
+        'peer_support/tests/fixtures/other_patients.json',
+    ]
 
     def setUp(self):
         self.mentor = Mentor.objects.create(username='test_mentor')
@@ -28,6 +34,15 @@ class HelpersViewTestCase(TestCase):
     def test_get_referral_code_no_referral(self):
         referral_code = get_referral_code(self.mentor)
         self.assertIsNone(referral_code)
+    
+    def test_get_addable_peers(self):
+        current_user = User.objects.get(username='@petrapickles')
+        admin = User.objects.get(username='@admin')
+        friend = User.objects.get(username='@peterpickles')
+        addable_peers = get_addable_peers(current_user)
+        self.assertNotIn(current_user, addable_peers)
+        self.assertNotIn(admin, addable_peers)
+        self.assertNotIn(friend, addable_peers)
 
     def tearDown(self):
         Mentor.objects.all().delete()
