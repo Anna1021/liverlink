306279d79ec8bed535d43fbf82a922ff9fbe5fe8
Yara Niroukh <yaraniroukh@hotmail.com>
2024-02-24 14:32:39 +0000

    Fix 'condition' field on MentorForm not having dropdown

--- peer_support/forms/mentor_form.py
+++ peer_support/forms/mentor_form.py
@@ -1,10 +1,12 @@
 from django import forms
 from peer_support.forms import PatientForm
 from peer_support.models import Mentor
+from .form_choices import CONDITION_CHOICES
 
 class MentorForm(forms.ModelForm):
     """Form to update mentor profiles."""
 
+    condition = forms.ChoiceField(choices=CONDITION_CHOICES, required=False)
     referral_code = forms.CharField(disabled=True, required=False)
 
     class Meta:
