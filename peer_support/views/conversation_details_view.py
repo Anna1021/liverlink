25eb52449fb16d0e9986dfbeb799722ec6d35547
Hannah Porteous <hannah.porteous@yahoo.com>
2024-02-27 11:55:36 +0000

    Fully implement adding users to conversations

--- peer_support/views/conversation_details_view.py
+++ peer_support/views/conversation_details_view.py
@@ -32,3 +32,18 @@ class ConversationDetailsView(LoginRequiredMixin,FormView):
             'form': AddUsersForm(current_user,conversation.as_group())
         }
         return render(request,self.template_name,context)
+
+    def post(self,request, conversation_id):
+        conversation = Conversation.objects.get(id=conversation_id)
+        form = AddUsersForm(request.user,conversation.as_group(),data=request.POST)
+        context  = {
+            'conversation':conversation.as_group(),
+            'user_conversations':request.user.sort_conversations(),
+            'form': AddUsersForm(request.user,conversation.as_group())
+        }
+        if form.is_valid():
+            form.save(conversation)
+            return render(request,self.template_name,context)
+        else:
+            messages.error(request,"You have to add at least 1 person")
+            return render(request,self.template_name,{'form':form,'conversation':conversation.as_group(),'user_conversations':request.user.sort_conversations()})#
