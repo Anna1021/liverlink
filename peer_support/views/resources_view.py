c24e76b4e6737f8551fc31100a2f7b712b659616
Yara Niroukh <yaraniroukh@hotmail.com>
2024-02-29 22:57:28 +0000

    Implement blocking functionality: users cannot see questions on 'resources' page from users they have blocked / have blocked them

--- peer_support/views/resources_view.py
+++ peer_support/views/resources_view.py
@@ -2,6 +2,7 @@ from peer_support.models import Question
 from django.shortcuts import render
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 @login_required
 def resources(request):
