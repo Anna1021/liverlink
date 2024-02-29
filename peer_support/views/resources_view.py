303e43ab2dbdb4ad47c1cf5bd7f054d38fe1b246
Yara Niroukh <yaraniroukh@hotmail.com>
2024-02-29 22:57:28 +0000

    Implement blocking functionality: users cannot see questions on 'resources' page from users they have blocked / have blocked them

--- peer_support/views/resources_view.py
+++ peer_support/views/resources_view.py
@@ -1,8 +1,10 @@
 from peer_support.models import Question
 from django.shortcuts import render
+from django.db.models import Q
 
 def resources(request):
     """display the first 10 questions from newest to oldest"""
     questions = Question.objects.order_by('-created_at')
+    questions = questions.exclude(Q(author__in=request.user.blocked_users.all()) | Q(author__in=request.user.blocked_by.all()))
     context = {'questions': questions}
     return render(request, 'resources.html', context)
