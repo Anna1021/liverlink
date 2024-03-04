cb81031141cd2f2f4f1fdd853faefbfdc3c0d83b
Yara Niroukh <yaraniroukh@hotmail.com>
2024-03-04 23:34:34 +0000

    Fix HTML bugs from main merge & fix bug with hiding messages

--- static/js/conversation.js
+++ static/js/conversation.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
         var blocked_messages = document.getElementsByName('blocked-message');
 
         for (var i = 0; i < blocked_messages.length; ++i) {
-            var message = blocked_messages[i]
+            let message = blocked_messages[i]
             message.innerHTML = "You have blocked this user. Click to reveal text."
             message.addEventListener('click', function() {
                 var message_text = message.getAttribute('data-text')
                 message.innerHTML = message_text
                 message.classList.remove('text-muted')
-            })
+            }.bind(this, message))
         }
     }
 });
