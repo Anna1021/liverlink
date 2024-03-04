74302472f3cf1805d15618d683c0aca23a6542fb
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-03-04 12:54:30 +0000

    Hard code default profile picture

--- peer_support/views/profile_update_view.py
+++ peer_support/views/profile_update_view.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
 from django.urls import reverse
-from peer_support.models import Patient, Parent, Mentor
+from peer_support.models import Patient, Parent, Mentor, UserProfile
 from peer_support.forms import UserForm, PatientForm, ParentForm, MentorForm
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
@@ -34,6 +34,12 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
         else:
             user = self.request.user
         return user
+    
+    def get_context_data(self, **kwargs):
+        """Return context data for the view."""
+        context = super().get_context_data(**kwargs)
+        context['user_profile'] = UserProfile.objects.get(user=self.request.user)
+        return context
 
     def get_success_url(self):
         """Return redirect URL after successful update."""
--- peer_support/views/profile_view.py
+++ peer_support/views/profile_view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from peer_support.models import User
+from peer_support.models import User, UserProfile
 
 class ProfileView(LoginRequiredMixin, View):
     """Displays other user's profile"""
@@ -10,12 +10,11 @@ class ProfileView(LoginRequiredMixin, View):
         """Get request for user to view profile"""
         user = User.objects.get(username=username)
         context = {'user': user}
-
         if hasattr(user, 'parent'):
             context['parent'] = user.parent
         elif hasattr(user, 'patient'):
             context['patient'] = user.patient
         else:
             context['mentor'] = user.mentor
-
+        context['user_profile'] = UserProfile.objects.get(user=user)
         return render(request, 'profile.html', context)
