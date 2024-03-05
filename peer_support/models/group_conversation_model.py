65432b4693b9fd26f1e053a18e26936dc1db41ca
Riya <k22007695@kcl.ac.uk>
2024-03-05 16:55:37 +0000

    Add changes done to both conversation models

--- peer_support/models/group_conversation_model.py
+++ peer_support/models/group_conversation_model.py
@@ -1,5 +1,6 @@
 from django.db import models
 from peer_support.models import Conversation
+
 class GroupConversation(Conversation):
     """Model used for group conversations between 2+ users"""
     name = models.CharField(max_length=20,null=True)
@@ -10,6 +11,19 @@ class GroupConversation(Conversation):
         user.conversations.remove(self)
         if self.users.count()==0:
             self.delete() 
+
+    def rename(self, new_name):
+        """Rename conversation"""
+        self.name = new_name
+        if new_name == '':
+            self.name = None
+        self.save()
+
+    def display_name(self):
+        """Name displayed in the form for renaming conversations"""
+        if self.name is None:
+            return ""
+        return self.name
 
     def __str__(self):
         """Return a string representing the display name of the conversation"""
--- peer_support/tests/models/test_group_conversation_model.py
+++ peer_support/tests/models/test_group_conversation_model.py
@@ -1,9 +1,9 @@
-"""Unit tests for the Conversation model."""
+"""Unit tests for the Group Conversation model."""
 from django.test import TestCase
 from peer_support.models import User,Message,Conversation
 
-class ConversationModelTestCase(TestCase):
-    """Unit tests for the Conversation model."""
+class GroupConversationModelTestCase(TestCase):
+    """Unit tests for the Group Conversation model."""
 
     fixtures = [
         'peer_support/tests/fixtures/default_user.json',
@@ -28,24 +28,45 @@ class ConversationModelTestCase(TestCase):
     def test_correct_internal_group_name(self):
         self.assertIsNone(self.group_conversation.name)
 
-    def test_correct_unset_group_name_displayed(self):
+    def test_correct_unset_group_names_displayed(self):
         display = str(self.group_conversation)
         self.assertEqual(display,"@johndoe, @peterpickles, @petrapickles")
+        display_name = self.group_conversation.display_name()
+        self.assertEqual(display_name,"")
 
-    def test_correct_set_group_name_displayed(self):
+    def test_correct_set_group_names_displayed(self):
         self.group_conversation.name = 'test'
         display = str(self.group_conversation)
         self.assertEqual(display,"test")
+        display_name = self.group_conversation.display_name()
+        self.assertEqual(display_name,"test")
 
     def test_add_user_to_group(self):
-        user2 = User.objects.get(pk=2)
-        self.group_conversation.add_user(user2)
+        user2 = User.objects.filter(pk=2)
+        self.group_conversation.add_users(user2)
         self.assertEqual(self.group_conversation.users.count(),4)
 
     def test_adding_existing_user_has_no_effect(self):
-        user = User.objects.get(pk=3)
-        self.group_conversation.add_user(user)
+        user = User.objects.filter(pk=3)
+        self.group_conversation.add_users(user)
         self.assertEqual(self.group_conversation.users.count(),3)
+
+    def test_sending_messages_adds_to_conversation(self):
+        before_count = self.group_conversation.messages.count()
+        new_message = Message.objects.create(
+            sender=self.user,
+            content='test',
+            send_time="2024-01-20T10:00:00Z"
+        )
+        self.conversation.send(new_message)
+        after_count = self.group_conversation.messages.count()
+        self.assertEqual(after_count,before_count+1)
+
+    def test_sending_messages_updates_last_updated(self):
+        time_before = self.group_conversation.last_updated
+        new_message = Message.objects.get(pk=2)
+        self.conversation.send(new_message)
+        self.assertNotEqual(time_before,self.group_conversation.last_updated)
 
     def test_user_not_in_group_when_user_deleted(self):
         self.assertIn(self.user,self.group_conversation.users.all())
@@ -58,6 +79,16 @@ class ConversationModelTestCase(TestCase):
             self.group_conversation.remove_user(user)
         after_count = Conversation.objects.count()
         self.assertEqual(after_count,before_count-1)
+
+    def test_renaming_group_to_specific_name(self):
+        new_name = 'Test'
+        self.group_conversation.rename(new_name)
+        self.assertEqual(self.group_conversation.name,'Test')
+
+    def test_renaming_to_blank(self):
+        new_name = ''
+        self.group_conversation.rename(new_name)
+        self.assertEqual(self.group_conversation.name,None)
     
     
     
