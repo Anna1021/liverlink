f606c712a03fbcffa05c2ccf7604e658abe4dcbe
Xinyao <k22008684@kcl.ac.uk>
2024-03-05 22:10:31 +0000

    remove themeChange list, add toggle function instead

--- static/js/dashboard_theme_switcher.js
+++ static/js/dashboard_theme_switcher.js
@@ -1,19 +1,20 @@
 $(document).ready(function () {
   var lightThemeLogo = '/static/images/Pulse-logo_black.png';
-  var darkThemeLogo ='/static/images/pulseLogoCropped.png';
+  var darkThemeLogo = '/static/images/pulseLogoCropped.png';
+  var moonLogo = '/static/images/moon.png';
+  var sunLogo = '/static/images/sun.png';
+  var darkBackground = '/static/images/background2.png';
+  var lightBackground = '/static/images/background_light.png';
 
   // Set image source based on theme and save to local storage
   function setImageSource(theme) {
     var logoImageSrc = theme === 'light-theme' ? lightThemeLogo : darkThemeLogo;
+    var themeImageSrc = theme === 'light-theme' ? sunLogo : moonLogo;
     $('.img-fluid').attr('src', logoImageSrc);
+    $('.theme-icon').attr('src', themeImageSrc);
     localStorage.setItem("currentLogo", logoImageSrc);
+    localStorage.setItem("currentThemeLogo", themeImageSrc);
   }
-
-  $('#theme').change(function() {
-    var theme = $(this).val();
-    setTheme(theme);
-    setImageSource(theme);
-  });
 
   function setTheme(themeName) {
     let mainDiv = $("#main");
@@ -23,7 +24,7 @@ $(document).ready(function () {
   }
 
   function getCurrentTheme() {
-    var themeSelected = $("#theme").val();
+    var themeSelected = $('#main').attr('class');
     console.log("themeSelected:", themeSelected);
     return themeSelected;
   }
@@ -32,20 +33,20 @@ $(document).ready(function () {
     localStorage.setItem("currentTheme", themeName);
   }
 
-  function selectTheme(themeName) {
-    $("#theme").val(themeName);
-  }
+  $('#theme').click(function() {
+    var currentTheme = getCurrentTheme() === 'light-theme' ? 'dark-theme' : 'light-theme'; // Toggle theme
+    setTheme(currentTheme); // Apply the theme
+    setImageSource(currentTheme); // Update image sources accordingly
+  });
 
   let storedTheme = localStorage.getItem("currentTheme");
-  let storedLogo = localStorage.getItem("currentLogo");
 
   if (storedTheme) {
     setTheme(storedTheme);
-    selectTheme(storedTheme);
     setImageSource(storedTheme); // Use stored theme to determine image source
   } else {
-    var currentTheme = getCurrentTheme();
-    setTheme(currentTheme);
-    setImageSource(currentTheme); // Use current theme to determine image source
+    var defaultTheme = 'light-theme'; // Default theme
+    setTheme(defaultTheme);
+    setImageSource(defaultTheme); // Use default theme to determine image source
   }
 });
