5ab6258a72081d4a7164f94e60ffb72337b35f27
Hannah Porteous <hannah.porteous@yahoo.com>
2024-03-06 19:22:42 +0000

    Fix comment replies

--- peer_support/models/post_comment_model.py
+++ peer_support/models/post_comment_model.py
@@ -9,13 +9,18 @@ from django.core.validators import RegexValidator
 
 class PostComment(models.Model):
     """Model for Post Comments"""
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='replies')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         ordering = ['-created_at']
+
+    def get_replies(self):
+        """Return replies"""
+        return PostComment.objects.filter(parent=self)
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.text}'
