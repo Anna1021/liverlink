5a01440a838907b5aadd9c49fe1e0db06e22676b
Hannah Porteous <hannah.porteous@yahoo.com>
2024-03-07 19:00:13 +0000

    Edit feed type toggle

--- static/js/feed.js
+++ static/js/feed.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleFeedButton = document.getElementById('toggleFeedButton');
+    
     const feedTypeInput = document.getElementById('feedType');
   
     toggleFeedButton.addEventListener('click', function() {
       if (feedTypeInput.value === 'global') {
         feedTypeInput.value = 'friends';
-        toggleFeedButton.textContent = 'Friends Only Feed';
       } else {
         feedTypeInput.value = 'global';
-        toggleFeedButton.textContent = 'Global Feed';
       }
   
       // Submit the form when the button is clicked
