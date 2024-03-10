aaead51412a0355dfc108638708c9607f49685e1
Katy Wakeman <katyjwakeman@gmail.com>
2024-03-10 16:47:49 +0000

    Add tests

--- peer_support/forms/helpers.py
+++ peer_support/forms/helpers.py
@@ -22,10 +22,10 @@ class NewPasswordMixin(forms.Form):
         cleaned_data = super().clean()
         new_password = self.cleaned_data.get('new_password')
         password_confirmation = self.cleaned_data.get('password_confirmation')
-        dob = self.cleaned_data.get('date_of_birth')
-        today = date.today()
         if new_password != password_confirmation:
             self.add_error('password_confirmation', 'Confirmation does not match password.')
-        if (dob.year + 13, dob.month, dob.day) > (today.year, today.month, today.day):
+        dob = self.cleaned_data.get('date_of_birth')
+        today = date.today()
+        if dob is not None and (dob.year + 13, dob.month, dob.day) > (today.year, today.month, today.day):
             self.add_error('date_of_birth', 'You must be 13 years old to register.')
         return cleaned_data
