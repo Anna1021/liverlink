cb63348c91536a275d7a7f9c0a5ae67aa08a8c97
Yara Niroukh <yaraniroukh@hotmail.com>
2024-03-10 22:08:58 +0000

    Restore profile tests lost in merge

--- peer_support/tests/views/test_profile_view.py
+++ peer_support/tests/views/test_profile_view.py
@@ -2,13 +2,17 @@
 from django.test import TestCase
 from django.urls import reverse
 from peer_support.tests.helpers import reverse_with_next
-from peer_support.models import User, FriendRequest
+from peer_support.models import User, FriendRequest, Report
+from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
+from django.contrib import messages
 
 class ProfileViewTestCase(TestCase):
     """Tests of the other user profile view."""
 
     fixtures = ['peer_support/tests/fixtures/default_user.json',
                 'peer_support/tests/fixtures/other_users.json',
+                'peer_support/tests/fixtures/default_admin.json',
                 'peer_support/tests/fixtures/default_parent.json',
                 'peer_support/tests/fixtures/other_patients.json',
                 'peer_support/tests/fixtures/other_mentors.json',
@@ -141,6 +145,54 @@ class ProfileViewTestCase(TestCase):
         self.assertTemplateUsed(response, 'profile.html')
         mentor = response.context['mentor']
         self.assertEqual(mentor, user.mentor)
+
+    def test_successful_report_profile(self):
+        user_to_report = User.objects.get(username='@janedoe')
+        user_content_type = ContentType.objects.get_for_model(user_to_report)
+        Report.objects.create(reporter=self.user, 
+                              reason='spam', 
+                              reported_at=timezone.now(), 
+                              content_type=user_content_type, 
+                              object_id=user_to_report.pk, 
+                              content_object=user_to_report
+                              )
+        url = reverse('profile', kwargs={'username': user_to_report.username})
+        report_data = {
+            'action': user_to_report.pk,
+            'reason': 'abuse'
+        }
+        response = self.client.post(url, data=report_data)
+        self.assertEqual(response.status_code, 302) 
+        content_type = ContentType.objects.get_for_model(User)
+        report_exists = Report.objects.filter(
+            content_type=content_type,
+            object_id=user_to_report.pk,
+            reason='abuse',
+            reporter=self.user
+        ).exists()
+        self.assertTrue(report_exists, "The report should exist in the database.")
+
+    def test_unsuccessful_report_profile(self):
+        user_to_report = User.objects.get(username='@janedoe')
+        user_content_type = ContentType.objects.get_for_model(user_to_report)
+        Report.objects.create(reporter=self.user, 
+                              reason='spam', 
+                              reported_at=timezone.now(), 
+                              content_type=user_content_type, 
+                              object_id=user_to_report.pk, 
+                              content_object=user_to_report
+                              )
+        url = reverse('profile', kwargs={'username': user_to_report.username})
+        valid_message_id = 1  
+        report_data = {
+            'action': valid_message_id,
+            'reason': 'dfdsdf'
+        }
+        response = self.client.post(url, data=report_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        messages_list = list(messages.get_messages(response.wsgi_request))
+        self.assertEqual(len(messages_list), 1)
+        self.assertIn("There was an issue with the report.", str(messages_list[0]))
 
     def test_get_profile_not_logged_in(self):
         self.client.logout()
