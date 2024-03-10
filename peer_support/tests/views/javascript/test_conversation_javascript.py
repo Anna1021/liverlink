87d3c6ee79373fe0ec5227c836626ca735a9e2d4
Riya <k22007695@kcl.ac.uk>
2024-03-10 17:34:42 +0000

    Add waits to js tests

--- peer_support/tests/views/javascript/test_conversation_javascript.py
+++ peer_support/tests/views/javascript/test_conversation_javascript.py
@@ -1,6 +1,9 @@
 """Unit tests of javascript in conversation view."""
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -20,6 +23,7 @@ class ConversationJavascriptTest(StaticLiveServerTestCase):
         cls.selenium = WebDriver(service=Service(), options=options)
         cls.selenium.maximize_window()
         cls.selenium.implicitly_wait(40)
+        cls.wait = WebDriverWait(cls.selenium, 20)
         
     @classmethod
     def tearDownClass(cls):
@@ -27,7 +31,6 @@ class ConversationJavascriptTest(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def test_hide_messages_from_blocked_users(self):
-
         user = User.objects.get(username='@janedoe')
         second_user = User.objects.get(username='@petrapickles')
         user.blocked_users.add(second_user)
@@ -42,22 +45,32 @@ class ConversationJavascriptTest(StaticLiveServerTestCase):
         conversation.messages.add(message)
         user.conversations.add(conversation)
     
-
         self.selenium.get('%s%s' % (self.live_server_url, '/log_in/'))
-        username_input = self.selenium.find_element(By.NAME, "username")
-        username_input.send_keys('@janedoe')
-        password_input = self.selenium.find_element(By.NAME, "password")
-        password_input.send_keys('Password123')
-        self.selenium.find_element(By.XPATH, '//input[@value="Log in"]').click()
+        try:
+            username_input = self.wait.until(EC.presence_of_element_located((By.NAME, "username")))
+            username_input.send_keys('@janedoe')
 
-        self.selenium.find_element(By.XPATH, "//button[contains(text(), 'Messages')]").click()
+            password_input = self.wait.until(EC.presence_of_element_located((By.NAME, "password")))
+            password_input.send_keys('Password123')
+            
+            login_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@value="Log in"]')))
+            login_button.click()
 
-        self.selenium.find_element(By.LINK_TEXT, "@petrapickles").click()
+            messages_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Messages')]")))
+            messages_button.click()
 
-        hidden_message = self.selenium.find_element(By.NAME, "blocked-message")
-        self.assertEqual("You have blocked this user. Click to reveal text.", hidden_message.get_attribute("innerHTML"))
-        self.assertEqual("text-muted", hidden_message.get_attribute("class"))
+            user_link = self.wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "@petrapickles")))
+            user_link.click()
 
-        hidden_message.click()
-        self.assertEqual(hidden_message.get_attribute("data-text"), hidden_message.get_attribute("innerHTML"))
-        self.assertEqual("", hidden_message.get_attribute("class"))
+            hidden_message = self.wait.until(EC.presence_of_element_located((By.NAME, "blocked-message")))
+
+            self.assertEqual("You have blocked this user. Click to reveal text.", hidden_message.get_attribute("innerHTML"))
+            self.assertEqual("text-muted", hidden_message.get_attribute("class"))
+
+            hidden_message.click()
+
+            self.assertEqual(hidden_message.get_attribute("data-text"), hidden_message.get_attribute("innerHTML"))
+            self.assertEqual("", hidden_message.get_attribute("class"))
+        
+        except TimeoutException as e:
+            self.fail(f"Test failed due to timeout while waiting for the question to be visible or interactable: {e}")
