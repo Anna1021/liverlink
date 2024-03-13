e07d98e2388071d49cc6c80dafda9a7d11e0b2d0
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-03-13 00:33:14 +0000

    Change customisation view to allow deletion of static_root

--- peer_support/views/customisation_view.py
+++ peer_support/views/customisation_view.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
+from django.contrib.staticfiles import finders
 
 class CustomisationView(LoginRequiredMixin, View):
     """Displays user's customisation settings"""
@@ -10,6 +11,13 @@ class CustomisationView(LoginRequiredMixin, View):
     def get(self, request):
         """Get request for user to view customisation settings"""
         
-        profile_pictures = os.listdir(os.path.join(settings.STATIC_ROOT, 'profile_pictures'))
+        profile_pictures = self.get_profile_pictures()
         context = {'profile_pictures': profile_pictures}
         return render(request, 'customisation.html', context)
+
+    def get_profile_pictures(self):
+        profile_pictures_dir = finders.find('profile_pictures', all=True)
+        profile_pictures = []
+        for dir in profile_pictures_dir:
+            profile_pictures.extend(os.listdir(dir))
+        return profile_pictures
--- static_root/js/conversation.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-
-    $('#conversation').animate(
-        {scrollTop:$('#conversation').prop('scrollHeight')});
-});
-
