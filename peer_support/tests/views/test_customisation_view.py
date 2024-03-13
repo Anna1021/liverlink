ebc12db9cbc6df81183d9214fe2696cc6a252f61
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-03-13 00:49:49 +0000

    Update customisation view tests

--- peer_support/tests/views/test_customisation_view.py
+++ peer_support/tests/views/test_customisation_view.py
@@ -1,8 +1,10 @@
-"""Tests for the customisation view."""
+import os
 from django.test import TestCase
 from django.urls import reverse
 from peer_support.tests.helpers import reverse_with_next
+from django.contrib.staticfiles import finders
 from peer_support.models import User
+from peer_support.views.customisation_view import CustomisationView
 
 class CustomisationViewTestCase(TestCase):
     """Tests for the customisation view."""
@@ -21,8 +23,15 @@ class CustomisationViewTestCase(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         profile_pictures = response.context['profile_pictures']
-        self.assertEqual(len(profile_pictures), 40)
+        self.assertIsInstance(profile_pictures, list)
         self.assertTemplateUsed(response, 'customisation.html')
+
+    def test_get_profile_pictures(self):
+        view = CustomisationView()
+        profile_pictures = view.get_profile_pictures()
+        self.assertIsInstance(profile_pictures, list)
+        expected_profile_pictures = os.listdir(finders.find('profile_pictures'))
+        self.assertEqual(set(profile_pictures), set(expected_profile_pictures))
 
     def test_customisation_without_being_logged_in(self):
         self.client.logout()
