cf4bc1e6ead19a85adefd655fac71182c5792759
Xinyao <k22008684@kcl.ac.uk>
2024-03-14 14:41:25 +0000

    refract reply_page_view

--- peer_support/views/reply_page_view.py
+++ peer_support/views/reply_page_view.py
@@ -1,30 +1,30 @@
-from peer_support.models import Question, Response
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
-from peer_support.forms import NewReplyForm
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect, reverse
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from peer_support.models import Question, Response
 from peer_support.forms import NewReplyForm
 
-@login_required(login_url='log_in')
-def reply_page(request): # break this down @anna
-    """Reply to the question"""
+class ReplyPageView(LoginRequiredMixin, View):
+    """reply to the question"""
+    
+    template_name = 'resources.html'
+    form_class = NewReplyForm
 
-    if request.method == 'POST':
-        form = NewReplyForm(request.POST)
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             question_id = request.POST.get('question')
             parent_id = request.POST.get('parent')
             reply = form.save(commit=False)
             reply.user = request.user
-            reply.question = Question(id=question_id)
+            reply.question = Question.objects.get(id=question_id)
             if parent_id:
-                reply.parent = Response(id=parent_id)
+                reply.parent = Response.objects.get(id=parent_id)
             reply.save()
             return redirect(f'/question/{question_id}#{reply.id}')
         else:
-            return render(request, 'resources.html', {'form': form})
-    else:
-        form = NewReplyForm()
-    return render(request, 'resources.html', {'form': form})
+            return render(request, self.template_name, {'form': form})
