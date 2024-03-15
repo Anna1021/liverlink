eb3082670d8aec580e5de04a502f2504a3e77e37
Hannah Porteous <hannah.porteous@yahoo.com>
2024-03-15 20:11:02 +0000

    Fix deleting other person's message

--- peer_support/models/report_model.py
+++ peer_support/models/report_model.py
@@ -7,7 +7,7 @@ from .model_choices import REPORT_CHOICES
 class Report(models.Model):
     """Model used for reporting objects to moderators (staff)"""
     reporter = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    reason = models.CharField(max_length=50,choices=REPORT_CHOICES)
+    reason = models.CharField(max_length=50,choices=REPORT_CHOICES,default='spam')
     reported_at = models.DateTimeField(auto_now_add=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
