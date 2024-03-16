4f6f4aa5b492937ab204148d542c2b047448aab8
Hannah Porteous <hannah.porteous@yahoo.com>
2024-03-16 16:23:34 +0000

    Remove read_by field from message model

--- peer_support/models/message_model.py
+++ peer_support/models/message_model.py
@@ -9,7 +9,6 @@ class Message(models.Model):
     content = models.CharField(max_length=100)
     send_time = models.DateTimeField(default=timezone.now)
     visible_to = models.ManyToManyField(User, blank=True, related_name='visible_to')
-    read_by = models.ManyToManyField(User, blank=True, related_name = 'read_by')
     previous_message = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
 
     def delete(self,users):
