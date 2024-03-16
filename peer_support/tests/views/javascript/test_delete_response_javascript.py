3639499c185f72270397afa8ee9ac6c802ce3907
Xinyao <k22008684@kcl.ac.uk>
2024-03-16 13:42:39 +0000

    git rid of the auto fill color

---
+++ peer_support/tests/views/javascript/test_delete_response_javascript.py
@@ -0,0 +1,66 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.alert import Alert
+
+class ReplyPageTest(StaticLiveServerTestCase):
+    """Unit test of javascript in reply_page view"""
+    fixtures = ['peer_support/tests/fixtures/default_user.json',
+                'peer_support/tests/fixtures/default_question.json',
+                'peer_support/tests/fixtures/default_response.json'
+                ]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = Options()
+        #options.add_argument("--headless") 
+        options.add_argument("--window-size=1920,1080")
+        cls.selenium = WebDriver(options=options)
+        cls.selenium.implicitly_wait(50)
+        cls.wait = WebDriverWait(cls.selenium, 20)
+        
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_reply_form_toggle(self):
+        try:
+            self.selenium.get(f'{self.live_server_url}/log_in/')
+            username_input =self.wait.until(EC.visibility_of_element_located((By.NAME, "username")))
+            username_input.send_keys('@johndoe')
+
+            password_input =self.wait.until(EC.visibility_of_element_located((By.NAME, "password")))
+            password_input.send_keys('Password123')
+
+            login_button =self.wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@value="Log in"]')))
+            login_button.click()
+
+            resources_button =self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Resources')]")))
+            resources_button.click()
+
+            link =self.wait.until(EC.element_to_be_clickable((By.XPATH, "//p[@class='question-list-item-title' and contains(text(), 'Sample Question Title')]")))
+            link.click()
+
+            delete_response_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='delete-response-button']")))
+            delete_response_button.click()
+
+            # Cancel the alert
+            self.wait.until(EC.alert_is_present())
+            self.selenium.switch_to.alert.dismiss()
+            delete_response_button.click()
+            self.wait.until(EC.alert_is_present())
+            self.selenium.switch_to.alert.accept()
+
+            # # Assert that the response is deleted
+            # my_element_list = self.selenium.find_elements(By.XPATH, "//p[@class='response-body' and contains(text(), 'This is a sample response body.')]")
+            # self.assertFalse(my_element_list, "The response was not deleted as expected.")
+        except TimeoutException as e:
+            self.fail(f"Test failed due to timeout while waiting for the question to be visible or interactable: {e}")
+
+            
