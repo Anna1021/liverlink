202e7ee0dd863a9519ae1be246607ba94fcc7055
Hannah Porteous <hannah.porteous@yahoo.com>
2024-03-18 01:15:43 +0000

    Tweak pull from main

--- peer_support/views/__init__.py
+++ peer_support/views/__init__.py
@@ -13,7 +13,7 @@ from .profile_deactivate_view import deactivate_user
 from .conversation_view import ConversationView
 from .peer_select_view import PeerSelectView
 from .create_conversation_view import CreateConversationView
-from .new_question_page_view import newQuestionPage
+from .new_question_page_view import new_question_page
 from .reply_page_view import ReplyPageView
 from .resources_view import resources
 from .question_page_view import QuestionPageView
