93eaae1d7d65d3995ba6ab3a6000eb7c3ba6acdb
Xinyao <k22008684@kcl.ac.uk>
2024-03-18 16:57:40 +0000

    add test to delete question

--- peer_support/views/delete_question_view.py
+++ peer_support/views/delete_question_view.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import Http404
 from peer_support.models import Question
 
 def delete_question(request, id):
-    question = get_object_or_404(Question, pk=id)
-    
-    if request.method == 'POST':
+    try:
+        question = Question.objects.get(pk=id)
+    except Question.DoesNotExist:
+        messages.error(request, 'The question does not exist.')
+        return redirect('resources') 
+
+    if request.method == 'GET':
         question.delete()
         messages.success(request, 'The question has been deleted successfully.')
         return redirect('resources')
     else:
         context = {'question': question}
-        return render(request, 'delete_question.html', context)
+        return render(request, 'resources.html', context)
+
