caab241a116554ae2b60c0407cf3ba49ab4bac72
Xinyao <k22008684@kcl.ac.uk>
2024-03-19 14:00:34 +0000

    fix small issue with deleting question

--- peer_support/views/delete_question_view.py
+++ peer_support/views/delete_question_view.py
@@ -2,21 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import Http404
 from peer_support.models import Question
-
 def delete_question(request, id):
     """Delete a question."""
-
+    
     try:
         question = Question.objects.get(pk=id)
     except Question.DoesNotExist:
         messages.error(request, 'The question does not exist.')
         return redirect('resources') 
-
-    if request.method == 'GET':
-        question.delete()
-        messages.success(request, 'The question has been deleted successfully.')
-        return redirect('resources')
-    else:
-        context = {'question': question}
-        return render(request, 'resources.html', context)
-
+    print(question)
+    question.delete()
+    messages.success(request, 'The question has been deleted successfully.')
+    print("deleteddd")
+    return redirect('resources')
