d580578fbb26e8168fe04a3a8f0b20b431784767
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-03-20 21:56:56 +0000

    Format code

--- peer_support/admin.py
+++ peer_support/admin.py
@@ -3,78 +3,71 @@ from .models import User, Patient, Parent, Mentor, Referral, UserProfile, Notifi
 
 class UserProfileInline(admin.StackedInline):
     """Configuration of the admin interface for user profiles."""
+
     """Adds the user profile interface to the user interface."""
     model = UserProfile
     can_delete = False
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for users."""
-    
-    ordering = ('username',)
+
+    ordering = ("username",)
     list_filter = ["is_active"]
     inlines = [UserProfileInline]
-    list_display = [
-        'id','username', "is_active" ]
+    list_display = ["id", "username", "is_active"]
+
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for patients."""
 
-    ordering = ('username',)
+    ordering = ("username",)
     list_filter = ["is_active"]
     inlines = [UserProfileInline]
-    list_display = [
-        'id','username', "is_active" 
-    ] 
+    list_display = ["id", "username", "is_active"]
+
 
 @admin.register(Parent)
 class ParentAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for parents."""
 
-    ordering = ('username',)
+    ordering = ("username",)
     list_filter = ["is_active"]
     inlines = [UserProfileInline]
-    list_display = [
-        'id','username', "is_active" 
-    ]
+    list_display = ["id", "username", "is_active"]
+
 
 @admin.register(Mentor)
 class MentorAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for mentors."""
 
-    ordering = ('username',)
+    ordering = ("username",)
     list_filter = ["is_active"]
     inlines = [UserProfileInline]
-    list_display = [
-        'id','username', "is_active" 
-    ]
+    list_display = ["id", "username", "is_active"]
+
 
 @admin.register(Referral)
 class ReferralAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for referrals."""
 
-    list_display = [
-        'referrer', 'code'
-    ]
+    list_display = ["referrer", "code"]
+
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for notifications."""
 
-    ordering = ('title',)
+    ordering = ("title",)
     list_filter = ["viewed", "created"]
-    list_display = [
-        'id', 'title', 'created', 'viewed', 'user'
-    ]
+    list_display = ["id", "title", "created", "viewed", "user"]
+
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for friend requests."""
 
     list_filter = ["reason", "reported_at"]
-    list_display = [
-        'reason','reported_at'
-    ]
-
-    
+    list_display = ["reason", "reported_at"]
