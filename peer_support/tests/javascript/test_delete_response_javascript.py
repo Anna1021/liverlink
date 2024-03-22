13a414315716f09d59b8798a016985361a23f941
Riya <k22007695@kcl.ac.uk>
2024-03-22 18:29:09 +0000

    Make Js test headless

--- peer_support/tests/javascript/test_delete_response_javascript.py
+++ peer_support/tests/javascript/test_delete_response_javascript.py
@@ -19,7 +19,7 @@ class DeleteResponseTest(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         options = Options()
-        #options.add_argument("--headless") 
+        options.add_argument("--headless") 
         options.add_argument("--window-size=1920,1080")
         cls.selenium = WebDriver(options=options)
         cls.selenium.implicitly_wait(50)
