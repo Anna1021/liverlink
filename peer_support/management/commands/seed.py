1370eaa9f89590ac315be6c41314accddeda5452
Yara Niroukh <yaraniroukh@hotmail.com>
2024-03-23 23:27:34 +0000

    Restore seed.py, with formatting

--- peer_support/management/commands/seed.py
+++ peer_support/management/commands/seed.py
@@ -102,6 +102,52 @@ parent_fixtures = [
     },
 ]
 
+professional_fixtures = [
+    {
+        'username': '@joanneclarke', 
+        'email': 'joanne.clarke@example.org', 
+        'first_name': 'Joanne', 
+        'last_name': 'Clarke', 
+        'date_of_birth': '1968-04-15', 
+        'gender': 'F', 
+        'location': 'US', 
+        'hospital': 'Blackpool Teaching Hospitals NHS Foundation Trust', 
+        'ethnicity': 'BR', 
+        'language': 'en', 
+        'bio': 'Hello, I am Joanne.', 
+        'expertise': 'Diabetes',
+        'referral_code':'ABC123'
+    },
+    {
+        'username': '@jeremybarnett', 
+        'email': 'jeremny.barnett@example.org', 
+        'first_name': 'Jeremy', 
+        'last_name': 'Barnett', 
+        'date_of_birth': '1995-11-2', 
+        'gender': 'M', 
+        'location': 'CA', 
+        'hospital': 'Countess of Chester Hospital NHS Foundation Trust', 
+        'ethnicity': 'BR', 
+        'language': 'en', 
+        'bio': 'Hey there, I am Jeremy.', 
+        'referral_code':'DEF456'
+    },
+    {
+        'username': '@paulaevans', 
+        'email': 'paula.evans@example.org', 
+        'first_name': 'Paula', 
+        'last_name': 'Evans', 
+        'date_of_birth': '1975-2-30', 
+        'gender': 'F', 
+        'location': 'AU', 
+        'hospital': 'Blackpool Teaching Hospitals NHS Foundation Trust', 
+        'ethnicity': 'BR', 
+        'language': 'en', 
+        'bio': 'Hi, I am Paula.', 
+        'referral_code':'GHI789'
+    },
+]
+
 mentor_fixtures = [
     {
         "username": "@sarahsmith",
@@ -111,6 +157,7 @@ mentor_fixtures = [
         "date_of_birth": "1992-05-15",
         "gender": "F",
         "location": "US",
+        "hospital": "Blackpool Teaching Hospitals NHS Foundation Trust",
         "ethnicity": "BR",
         "language": "en",
         "bio": "Hello, I am Sarah.",
@@ -126,6 +173,7 @@ mentor_fixtures = [
         "date_of_birth": "1985-09-20",
         "gender": "M",
         "location": "CA",
+        "hospital": "Countess of Chester Hospital NHS Foundation Trust",
         "ethnicity": "BR",
         "language": "en",
         "bio": "Hey there, I am David.",
@@ -140,54 +188,12 @@ mentor_fixtures = [
         "date_of_birth": "1978-12-03",
         "gender": "F",
         "location": "AU",
+        "hospital": "Blackpool Teaching Hospitals NHS Foundation Trust",
         "ethnicity": "BR",
         "language": "en",
         "bio": "Hi, I am Emily.",
         "age_of_diagnosis": 3,
         "referral_code": "GHI789",
-    },
-]
-
-professional_fixtures = [
-    {
-        'username': '@joanneclarke', 
-        'email': 'joanne.clarke@example.org', 
-        'first_name': 'Joanne', 
-        'last_name': 'Clarke', 
-        'date_of_birth': '1968-04-15', 
-        'gender': 'F', 
-        'location': 'US', 
-        'ethnicity': 'BR', 
-        'language': 'en', 
-        'bio': 'Hello, I am Joanne.', 
-        'expertise': 'Diabetes',
-        'referral_code':'ABC123'
-    },
-    {
-        'username': '@jeremybarnett', 
-        'email': 'jeremny.barnett@example.org', 
-        'first_name': 'Jeremy', 
-        'last_name': 'Barnett', 
-        'date_of_birth': '1995-11-2', 
-        'gender': 'M', 
-        'location': 'CA', 
-        'ethnicity': 'BR', 
-        'language': 'en', 
-        'bio': 'Hey there, I am Jeremy.', 
-        'referral_code':'DEF456'
-    },
-    {
-        'username': '@paulaevans', 
-        'email': 'paula.evans@example.org', 
-        'first_name': 'Paula', 
-        'last_name': 'Evans', 
-        'date_of_birth': '1975-2-30', 
-        'gender': 'F', 
-        'location': 'AU', 
-        'ethnicity': 'BR', 
-        'language': 'en', 
-        'bio': 'Hi, I am Paula.', 
-        'referral_code':'GHI789'
     },
 ]
 
