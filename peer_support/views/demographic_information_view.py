eb07c42d523f4817048e2804255c5dd152df10ba
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2024-03-24 21:23:11 +0000

    Minor formatting changes

--- peer_support/views/demographic_information_view.py
+++ peer_support/views/demographic_information_view.py
@@ -7,6 +7,7 @@ from django.views import View
 from collections import defaultdict
 from collections import Counter
 
+
 class DemographicInformationView(LoginRequiredMixin, View):
     """Displays demographic inforamtion for all users to staff"""
 
@@ -14,7 +15,7 @@ class DemographicInformationView(LoginRequiredMixin, View):
         current_user = request.user
         if not current_user.is_staff:
             messages.error(request, "You do not have access to this view.")
-            return redirect(reverse('feed'))
+            return redirect(reverse("feed"))
         context = {
             'user_types_labels': list(self.get_user_types().keys()),
             'user_types_count': list(self.get_user_types().values()),
@@ -50,35 +51,39 @@ class DemographicInformationView(LoginRequiredMixin, View):
             'antarctica_count': list(self.get_locations_specific().get("Antarctica", {}).values())
         }
         return render(request, 'demographic_information.html', context)
-    
+
     def get_professional_expertise(self):
         professional_expertise = Professional.objects.values_list('expertise', flat=True)
         professional_expertise = [map_blank_key(expertise) for expertise in professional_expertise]
         return Counter(professional_expertise) 
-    
+
     def get_mentor_conditions(self):
         mentor_conditions = Mentor.objects.values_list('condition', flat=True)
         mentor_conditions = [map_blank_key(condition) for condition in mentor_conditions]
         return Counter(mentor_conditions) 
-    
+
     def get_age_ranges(self):
-        users_ages = [calculate_age(user.date_of_birth) for user in User.objects.all() if user.date_of_birth is not None]
+        users_ages = [
+            calculate_age(user.date_of_birth)
+            for user in User.objects.all()
+            if user.date_of_birth is not None
+        ]
         age_ranges = {"13-20": 0, "21-30": 0, "31-40": 0, "41-50": 0, "51-60": 0, "61-70": 0, "71+":0}
         for age in users_ages:
             if 13 <= age <= 20:
-                age_ranges["13-20"]+=1
+                age_ranges["13-20"] += 1
             elif 21 <= age <= 30:
-                age_ranges["21-30"]+=1
+                age_ranges["21-30"] += 1
             elif 31 <= age <= 40:
-                age_ranges["31-40"]+=1
+                age_ranges["31-40"] += 1
             elif 41 <= age <= 50:
-                age_ranges["41-50"]+=1
+                age_ranges["41-50"] += 1
             elif 51 <= age <= 60:
-                age_ranges["51-60"]+=1
+                age_ranges["51-60"] += 1
             elif 61 <= age <= 70:
-                age_ranges["61-70"]+=1
+                age_ranges["61-70"] += 1
             else:
-                age_ranges["71+"]+=1
+                age_ranges["71+"] += 1
         return age_ranges
 
     def get_user_types(self):
@@ -86,8 +91,12 @@ class DemographicInformationView(LoginRequiredMixin, View):
         parents = Parent.objects.count()
         mentors = Mentor.objects.count()
         professionals = Professional.objects.count()
-        return {'patients': patients,
-            'parents': parents, 'mentors': mentors, 'professional': professionals}
+        return {
+            "patients": patients,
+            "parents": parents,
+            "mentors": mentors,
+            "professional": professionals,
+        }
 
     def get_user_ethnicities(self):
         users = User.objects.all()
@@ -120,10 +129,13 @@ class DemographicInformationView(LoginRequiredMixin, View):
     def get_locations_specific(self):
         """Returns set of all countries for each continent and number of users in each"""
 
-        country_codes = User.objects.values_list('location', flat=True)
+        country_codes = User.objects.values_list("location", flat=True)
         continent_to_countries = defaultdict(list)
         for code in country_codes:
             continent, country = country_to_continent_specific(code)
             continent_to_countries[continent].append(country)
-        continent_counts = {continent: Counter(countries) for continent, countries in continent_to_countries.items()}
-        return(continent_counts)
+        continent_counts = {
+            continent: Counter(countries)
+            for continent, countries in continent_to_countries.items()
+        }
+        return continent_counts
