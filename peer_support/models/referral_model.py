1b35f25ae23b51b34a4086cc16c4d3464762ca04
Katy Wakeman <katyjwakeman@gmail.com>
2024-03-26 12:02:39 +0000

    Fix referral codes

--- peer_support/models/referral_model.py
+++ peer_support/models/referral_model.py
@@ -3,5 +3,6 @@ from peer_support.models import Professional
 
 class Referral(models.Model):
     """Model used for referral related information."""
+    
     referrer = models.ForeignKey(Professional, related_name='referrals_made', on_delete=models.CASCADE)
     code = models.CharField(max_length=10, unique=True)
