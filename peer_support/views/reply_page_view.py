154f08341fcc61d100399214a220805f3464d189
Riya <k22007695@kcl.ac.uk>
2024-03-26 13:13:31 +0000

    Fix max_depth viewing

--- peer_support/views/reply_page_view.py
+++ peer_support/views/reply_page_view.py
@@ -4,17 +4,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from peer_support.models import Question, Response, Notification
 from peer_support.forms import NewReplyForm
 
-MAX_DEPTH = 10
+
 class ReplyPageView(LoginRequiredMixin, View):
     """Allows users to reply to a question"""
 
-
     template_name = 'resources.html'
     form_class = NewReplyForm
+    max_depth = 10
 
     def get(self, request):
         form = self.form_class()
-        return render(request, self.template_name, {'form': form, 'MAX_DEPTH': MAX_DEPTH})
+        return render(request, self.template_name, {'form': form, 'MAX_DEPTH': self.max_depth})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -23,8 +23,8 @@ class ReplyPageView(LoginRequiredMixin, View):
             parent_id = request.POST.get('parent')
             if parent_id:
                 parent_response = Response.objects.get(id=parent_id)
-                if parent_response.get_depth() >= MAX_DEPTH:
-                    return render(request, 'question.html', {'form': form, 'MAX_DEPTH': MAX_DEPTH , 'error': 'Maximum reply depth exceeded.'})
+                if parent_response.get_depth() >= self.max_depth:
+                    return render(request, 'question.html', {'form': form, 'MAX_DEPTH': self.max_depth , 'error': 'Maximum reply depth exceeded.'})
                 
             reply = form.save(commit=False)
             reply.user = request.user
@@ -33,7 +33,7 @@ class ReplyPageView(LoginRequiredMixin, View):
                  reply.parent = Response.objects.get(id=parent_id)
             reply.save()
             self.send_notification(reply)
-            return render(request, 'question.html', {'form': form, 'MAX_DEPTH': MAX_DEPTH})
+            return render(request, 'question.html', {'form': form, 'MAX_DEPTH': self.max_depth})
         else:
             return render(request, 'resources.html', {'form': form})
         
